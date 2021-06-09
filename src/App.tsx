2b100859af9942d7f00f77dc5b168f800702176b
ChandrakantPal <sanjay161097@gmail.com>
2021-06-09 22:43:19 +0530

    routing steup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+
 const App = () => {
-  return <div>hello world</div>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" component={Signup} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
