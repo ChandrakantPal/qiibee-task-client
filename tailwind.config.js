2255a675fccaa2bca97b56fd0061708711d9ce0f
ChandrakantPal <sanjay161097@gmail.com>
2021-06-10 19:52:59 +0530

    colors and size added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        108: '27rem',
+      },
+      colors: {
+        cerulean: {
+          DEFAULT: '#00A0E3',
+          50: '#C9EFFF',
+          100: '#B0E8FF',
+          200: '#7DD9FF',
+          300: '#4ACAFF',
+          400: '#17BBFF',
+          500: '#00A0E3',
+          600: '#007CB0',
+          700: '#00587D',
+          800: '#00344A',
+          900: '#001017',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
