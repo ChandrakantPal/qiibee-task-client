887c7a07279fb21a0be36d27b1a94aeabb683cae
ChandrakantPal <sanjay161097@gmail.com>
2021-06-11 23:54:41 +0530

    home page created

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="relative flex min-h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-white">
+        name
+        <nav>nav</nav>
+      </aside>
+      {/* content */}
+      <div>content goes here</div>
+    </div>
+  )
+}
+
+export default Home
