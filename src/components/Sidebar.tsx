a5441e46533e4b726436a6dbfdb1c549ee295e52
ChandrakantPal <sanjay161097@gmail.com>
2021-06-13 01:00:29 +0530

    mobile sidebar added and position bugs fixed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -76,64 +76,67 @@ const Sidebar = () => {
         </nav>
       </aside>
       {open && (
-        <aside className="z-10 w-1/2 h-screen py-5 space-y-6 text-sm transition duration-200 ease-in-out bg-white md:hidden">
-          <div className="flex flex-col justify-around h-18">
-            <Link to="/" className="p-5">
-              <span className="object-contain w-12 p-3 mr-2 text-center text-white rounded-full bg-gradient-to-b from-blue-500 to-pink-500">
-                CP
-              </span>
-              Chandrakant Pal
-            </Link>
-            <Divider />
-          </div>
-          <nav>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <AdjustmentsIcon className="object-contain w-5 mr-2" /> Set up
-              Dashboard
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <ChipIcon className="object-contain w-5 mr-2" /> Overview
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <DatabaseIcon className="object-contain w-5 mr-2" />
-              Loyalty Token
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <CodeIcon className="object-contain w-5 mr-2" /> Developers
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <TerminalIcon className="object-contain w-5 mr-2" /> Go to Sandbox
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <CogIcon className="object-contain w-5 mr-2" /> Admin
-            </Link>
-            <Link
-              to="/"
-              className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-            >
-              <QuestionMarkCircleIcon className="object-contain w-5 mr-2" />{' '}
-              Help Center
-            </Link>
-          </nav>
-        </aside>
+        <>
+          <aside className="fixed z-20 w-2/3 h-full py-5 space-y-6 text-sm transition duration-200 ease-in-out bg-white md:hidden">
+            <div className="flex flex-col justify-around h-18">
+              <Link to="/" className="p-5">
+                <span className="object-contain w-12 p-3 mr-2 text-center text-white rounded-full bg-gradient-to-b from-blue-500 to-pink-500">
+                  CP
+                </span>
+                Chandrakant Pal
+              </Link>
+              <Divider />
+            </div>
+            <nav>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <AdjustmentsIcon className="object-contain w-5 mr-2" /> Set up
+                Dashboard
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <ChipIcon className="object-contain w-5 mr-2" /> Overview
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <DatabaseIcon className="object-contain w-5 mr-2" />
+                Loyalty Token
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <CodeIcon className="object-contain w-5 mr-2" /> Developers
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <TerminalIcon className="object-contain w-5 mr-2" /> Go to
+                Sandbox
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <CogIcon className="object-contain w-5 mr-2" /> Admin
+              </Link>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
+              >
+                <QuestionMarkCircleIcon className="object-contain w-5 mr-2" />{' '}
+                Help Center
+              </Link>
+            </nav>
+          </aside>
+        </>
       )}
       <button
         type="button"
