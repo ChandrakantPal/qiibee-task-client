4514406776d0402ca71892fac2a84804791b63da
ChandrakantPal <sanjay161097@gmail.com>
2021-06-12 23:39:16 +0530

    nav items added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,29 @@ const Home = () => {
           </Link>
           <Divider />
         </div>
-        <nav>nav</nav>
+        <nav>
+          <Link to="/" className="block px-4 py-2 5">
+            Set up Dashboard
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Overview
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Loyalty Token
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Developers
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Go to Sandbox
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Admin
+          </Link>
+          <Link to="/" className="block px-4 py-2 5">
+            Help Center
+          </Link>
+        </nav>
       </aside>
       {/* content */}
       <div className="flex-1 p-10 text-2xl font-bold">content goes here</div>
