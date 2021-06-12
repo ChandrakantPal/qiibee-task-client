58b3562793e13b836f001ae89019c54904858bfe
ChandrakantPal <sanjay161097@gmail.com>
2021-06-13 01:00:58 +0530

    content area classes changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,79 +1,14 @@
-import { Link } from 'react-router-dom'
-import Divider from '../components/Divider'
-import {
-  AdjustmentsIcon,
-  ChipIcon,
-  DatabaseIcon,
-  CodeIcon,
-  TerminalIcon,
-  CogIcon,
-  QuestionMarkCircleIcon,
-} from '@heroicons/react/outline'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   return (
-    <div className="relative flex min-h-screen">
+    <div className="relative w-full h-screen md:flex">
       {/* Sidebar */}
-      <aside className="w-64 py-5 space-y-6 bg-white">
-        <div className="flex flex-col justify-around h-18">
-          <Link to="/" className="p-5">
-            <span className="object-contain w-12 p-3 mr-2 text-center text-white rounded-full bg-gradient-to-b from-blue-500 to-pink-500">
-              CP
-            </span>
-            Chandrakant Pal
-          </Link>
-          <Divider />
-        </div>
-        <nav>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <AdjustmentsIcon className="object-contain w-5 mr-2" /> Set up
-            Dashboard
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <ChipIcon className="object-contain w-5 mr-2" /> Overview
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <DatabaseIcon className="object-contain w-5 mr-2" />
-            Loyalty Token
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <CodeIcon className="object-contain w-5 mr-2" /> Developers
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <TerminalIcon className="object-contain w-5 mr-2" /> Go to Sandbox
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <CogIcon className="object-contain w-5 mr-2" /> Admin
-          </Link>
-          <Link
-            to="/"
-            className="flex items-center px-4 py-2.5 transition duration-200 hover:text-cerulean"
-          >
-            <QuestionMarkCircleIcon className="object-contain w-5 mr-2" /> Help
-            Center
-          </Link>
-        </nav>
-      </aside>
+      <Sidebar />
       {/* content */}
-      <div className="flex-1 p-10 text-2xl font-bold">content goes here</div>
+      <div className="w-full h-full p-10 text-2xl font-bold">
+        content goes here
+      </div>
     </div>
   )
 }
