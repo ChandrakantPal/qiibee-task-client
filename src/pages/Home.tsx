26808466fb52f4d99ff9a72112fe22ac21056f47
ChandrakantPal <sanjay161097@gmail.com>
2021-06-13 21:35:17 +0530

    content classes changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,11 @@ const Home = () => {
       {/* Sidebar */}
       <Sidebar />
       {/* content */}
-      <div className="w-full h-full p-10 text-2xl font-bold">
-        content goes here
+      <div className="w-full h-full text-2xl font-bold">
+        <div className="h-8 bg-gradient-to-r from-cerulean to-pink-500"></div>
+        <div className="container p-10 mx-auto text-center">
+          content goes here
+        </div>
       </div>
     </div>
   )
