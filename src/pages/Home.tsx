5b9d601e2b57179fe7821580c6f1940d722d26cc
ChandrakantPal <sanjay161097@gmail.com>
2021-06-15 18:09:06 +0530

    brand cards added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,24 @@
 import Sidebar from '../components/Sidebar'
+import BrandCard from '../components/BrandCard'
 
 const Home = () => {
   return (
-    <div className="relative w-full h-screen md:flex">
+    <div className="relative w-full h-full md:flex">
       {/* Sidebar */}
-      <Sidebar />
+      <div className="fixed h-full">
+        <Sidebar />
+      </div>
       {/* content */}
       <div className="w-full h-full text-2xl font-bold">
         <div className="h-8 bg-gradient-to-r from-cerulean to-pink-500"></div>
         <div className="container p-10 mx-auto text-center">
-          content goes here
+          <BrandCard />
+          <BrandCard />
+          <BrandCard />
+          <BrandCard />
+          <BrandCard />
+          <BrandCard />
+          <BrandCard />
         </div>
       </div>
     </div>
