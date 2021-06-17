e1b6bce7b4eb1be042df917878b22fac6e2b72e3
ChandrakantPal <sanjay161097@gmail.com>
2021-06-17 23:54:31 +0530

    brand route added

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import ApolloProvider from './ApolloProvider'
 import CustomerProfile from './pages/CustomerProfile'
+import BrandProfile from './pages/BrandProfile'
 
 const App = () => {
   return (
     <ApolloProvider>
       <BrowserRouter>
         <Switch>
+          <Route path="brand" component={BrandProfile} />
           <Route path="/customer" component={CustomerProfile} />
           <Route exact path="/" component={Home} />
           <Route exact path="/signup" component={Signup} />
