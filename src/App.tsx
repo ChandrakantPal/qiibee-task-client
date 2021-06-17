bd0df9ef892b52fc8a55180b2c6a54bb7e3696ee
ChandrakantPal <sanjay161097@gmail.com>
2021-06-17 23:34:27 +0530

    consumer route added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import ApolloProvider from './ApolloProvider'
+import CustomerProfile from './pages/CustomerProfile'
 
 const App = () => {
   return (
     <ApolloProvider>
       <BrowserRouter>
         <Switch>
+          <Route path="/customer" component={CustomerProfile} />
           <Route exact path="/" component={Home} />
           <Route exact path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
