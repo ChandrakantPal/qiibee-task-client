1b8a024c9cbf3b1352acc0deb149386ab3dcefd8
ChandrakantPal <sanjay161097@gmail.com>
2021-06-19 01:20:44 +0530

    hosted link added

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -7,7 +7,7 @@ import {
 import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/',
+  uri: 'https://qiibee-server.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
